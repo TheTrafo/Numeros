c82ce9e7df9e2a34bf1da07edc3a316779cd4864
TheTrafo <veselkapavel5@gmail.com>
2026-02-05 20:18:18 +0100

    cosmetic fixes, more accurate to the task description

--- src/Main.java
+++ src/Main.java
@@ -14,30 +14,33 @@ public class Main {
         int i, first = 0, second = 1;
         final int amountN = 21;
 
-        double aggregate = 0;
-        List<Double> numbers = new ArrayList<>(amountN);
+        double sum = 0;
+        List<Integer> numbers = new ArrayList<>(amountN);
 
-        for(i=0; i < amountN; i++){
-            System.out.print(first + " ");
-            numbers.add((double) first);
-            aggregate += first;
+        for(i = 0; i < amountN; i++) {
+            if (i != 0) {
+                System.out.print(" ");
+            }
+            System.out.print(first);
+            numbers.add(first);
+            sum += first;
 
             int third = second + first;
             first = second;
             second = third;
         }
-        double mean = aggregate/amountN;
+        double mean = sum / amountN;
         System.out.printf("\nThe mean is %.2f", mean);
 
         int middle = numbers.size() / 2;
-        double median;
+        int median;         //If the count of numbers is even, we would need to change from integer to float/double
 
-        if(numbers.size() % 2 == 0){
+        if (numbers.size() % 2 == 0) {
             median = (numbers.get(middle - 1) + numbers.get(middle)) / 2;
         } else {
             median = numbers.get(middle);
         }
-        System.out.printf("\nThe median is %.2f", median);
+        System.out.printf("\nThe median is " + median);
     }
 
 }
